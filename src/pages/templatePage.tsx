cc1cc1956bf9d852d0fb512cc6bef5218e1bb297
matin turkaman <79017406+matinturkaman@users.noreply.github.com>
2022-02-05 10:40:15 -0500

    template title added

--- src/pages/templatePage.tsx
+++ src/pages/templatePage.tsx
@@ -1,9 +1,17 @@
-
-
 const TemplatePage = () => {
-    return (
-        <div>this is template page</div>
-    )
-}
+  return (
+    <div className="p-10">
+      <div className="text-center">
+        <p className="font-bold text-5xl text-primary-400">
+          The templates 
+        </p>
+        <p className="font-bold text-5xl text-primary-400">we have</p>
+        <p className="font-medium mt-6" style={{ color: "#9B8E88" }}>
+          You can use these templates
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default TemplatePage;
