4036b8b9b4dadea0a12382b1a2d0e539722675fc
matin turkaman <79017406+matinturkaman@users.noreply.github.com>
2022-02-05 13:16:16 -0500

    template page

--- src/pages/templatePage.tsx
+++ src/pages/templatePage.tsx
@@ -1,14 +1,17 @@
+import templateOne from "../svg/templateOne.svg";
+
 const TemplatePage = () => {
   return (
     <div className="p-10">
       <div className="text-center">
-        <p className="font-bold text-5xl text-primary-400">
-          The templates 
-        </p>
+        <p className="font-bold text-5xl text-primary-400">The templates</p>
         <p className="font-bold text-5xl text-primary-400">we have</p>
         <p className="font-medium mt-6" style={{ color: "#9B8E88" }}>
           You can use these templates
         </p>
+        <div className="mt-16">
+          <img src={templateOne} alt="" />
+        </div>
       </div>
     </div>
   );
