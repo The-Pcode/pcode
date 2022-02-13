24437b43d39eb8f8e2e341c84c5bba74c2844835
matin turkaman <79017406+matinturkaman@users.noreply.github.com>
2022-02-13 14:01:59 -0500

    icon added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
